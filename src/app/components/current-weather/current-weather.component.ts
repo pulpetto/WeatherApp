eb46e697121ab6d9a1d6c5dc35e9e8b951df5aab
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-07-26 22:34:15 +0200

    import service & further instructions

--- src/app/components/current-weather/current-weather.component.ts
+++ src/app/components/current-weather/current-weather.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { GeoLocationService } from 'src/app/services/geo-location.service';
 
 @Component({
-  selector: 'app-current-weather',
-  templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+    selector: 'app-current-weather',
+    templateUrl: './current-weather.component.html',
+    styleUrls: ['./current-weather.component.css'],
 })
 export class CurrentWeatherComponent {
+    constructor(private geolocationService: GeoLocationService) {}
 
+    userLocation = this.geolocationService.userLocation;
 }

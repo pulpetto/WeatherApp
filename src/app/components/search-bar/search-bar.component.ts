db48918c9898071015272ac81d1fd4c6e0ef1f53
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-07-26 17:45:19 +0200

    btn click

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-search-bar',
-  templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+    selector: 'app-search-bar',
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
-
+    onCitySearch(cityName: string) {
+        if (!cityName) return;
+    }
 }

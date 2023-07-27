54d6d96ceaac4b26928e71bb155322b9f09357b5
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-07-27 09:58:53 +0200

    styling pt. 2

--- src/app/components/tabbed-info/tabbed-info.component.ts
+++ src/app/components/tabbed-info/tabbed-info.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tabbed-info',
-  templateUrl: './tabbed-info.component.html',
-  styleUrls: ['./tabbed-info.component.css']
+    selector: 'app-tabbed-info',
+    templateUrl: './tabbed-info.component.html',
+    styleUrls: ['./tabbed-info.component.css'],
 })
-export class TabbedInfoComponent {
-
-}
+export class TabbedInfoComponent {}
